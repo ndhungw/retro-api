b6370b238b855802b61c840ca9aa82f3de59e0ed
ndhungw <ndh1379@gmail.com>
2020-10-27 23:15:54 +0700

    upload .env

--- db/database.js
+++ db/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqfsn.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
 class Database {
   constructor() {
     this._connect();
@@ -16,21 +15,6 @@ class Database {
       console.log("Connect to database successfully");
     });
   }
-
-  // _connect() {
-  //   mongoose
-  //     .connect(DB_URI, {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //       useCreateIndex: true,
-  //     })
-  //     .then(() => {
-  //       console.log("MongoDB database connection established successfully!");
-  //     })
-  //     .catch((err) => {
-  //       console.log("MongoDB database connection error!");
-  //     });
-  // }
 }
 
 module.exports = new Database();
